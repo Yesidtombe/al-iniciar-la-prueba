313629de84963d2e645497bd01ffc5643e3aee3d
Yesid Tombe <yesidtom@unicauca.edu.co>
2021-03-03 23:55:52 -0500

    Initializing project Test Android Developer

---
+++ app/src/main/java/com/system/napoleon/testnapoleon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.system.napoleon.testnapoleon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
