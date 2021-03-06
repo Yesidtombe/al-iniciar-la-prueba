5d609ef787618d52c799533d64b1eae53eb35dc0
Yesid Tombe <yesidtom@unicauca.edu.co>
2021-03-06 17:59:54 -0500

    Template post, main and detail layouts designed. Internationalization

--- app/src/main/java/com/system/napoleon/testnapoleon/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/system/napoleon/testnapoleon/view/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.system.napoleon.testnapoleon
+package com.system.napoleon.testnapoleon.view.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.system.napoleon.testnapoleon.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
